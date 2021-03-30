947694f8ddbdd8e17bc73b1ac6363e6a26759d00
Wojciech <womielczarek@gmail.com>
2021-03-30 11:42:07 +0200

    Working BotttomNavigation with fragments

--- app/src/main/java/com/example/qrscanner_appproject/MainActivity.java
+++ app/src/main/java/com/example/qrscanner_appproject/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.qrscanner_appproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bottom navigation
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        //Which item in bottom_nav should be checked by default
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        }
+
+        QrScannerFragment qrScannerFragment = new QrScannerFragment();
+        PatientsFragment patientsFragment = new PatientsFragment();
+        UserProfileFragment userProfileFragment = new UserProfileFragment();
+        MainFragment mainFragment = new MainFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.navigation_home:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.fragmentContainer, mainFragment).commit();
+                return true;
+
+            case R.id.navigation_scanner:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.fragmentContainer, qrScannerFragment).commit();
+                return true;
+
+            case R.id.navigation_patients:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.fragmentContainer, patientsFragment).commit();
+                return true;
+
+            case R.id.navigation_profile:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.fragmentContainer, userProfileFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
+
+
