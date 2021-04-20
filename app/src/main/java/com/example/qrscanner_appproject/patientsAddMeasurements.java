5a0c5b42c32c79254ec34d9ae35b7ed923e1b54d
Wojciech <womielczarek@gmail.com>
2021-04-20 12:49:00 +0200

    Added and implemented fragment_patients_add_measurements

---
+++ app/src/main/java/com/example/qrscanner_appproject/patientsAddMeasurements.java
@@ -0,0 +1,114 @@
+package com.example.qrscanner_appproject;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+
+public class patientsAddMeasurements extends Fragment {
+
+    Button dateButton, timeButton;
+    TextView dateTextView, timeTextView;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_patients_add_measurements, container, false);
+
+        dateButton = view.findViewById(R.id.dateButton);
+        timeButton = view.findViewById(R.id.timeButton);
+        dateTextView = view.findViewById(R.id.dateTextView);
+        timeTextView = view.findViewById(R.id.timeTextView);
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleDateButton();
+            }
+        });
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleTimeButton();
+            }
+        });
+
+        return view;
+    }
+
+    private void handleTimeButton() {
+        Calendar calendar = Calendar.getInstance();
+        int HOUR = calendar.get(Calendar.HOUR_OF_DAY);
+        int MINUTE = calendar.get(Calendar.MINUTE);
+
+        boolean is24HourFormat = DateFormat.is24HourFormat(this.getContext());
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this.getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                String timeString = hourOfDay + ":" + minute;
+//                timeTextView.setText(timeString);
+
+                //Time formatting
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar1.set(Calendar.MINUTE, minute);
+
+                CharSequence charSequence = DateFormat.format("hh:mm a", calendar1);
+                timeTextView.setText(charSequence);
+            }
+        }, HOUR, MINUTE, is24HourFormat);
+
+        timePickerDialog.show();
+    }
+
+    private void handleDateButton() {
+
+        Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this.getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int date) {
+//                String dateString = year + "/" + month + "/" + date;
+//                dateTextView.setText(dateString);
+
+                //Date formatting
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.YEAR, year);
+                calendar1.set(Calendar.MONTH, month);
+                calendar1.set(Calendar.DATE, date);
+
+                CharSequence dateCharSequence = DateFormat.format("EEEE, dd MMM yyy", calendar1);
+                dateTextView.setText(dateCharSequence);
+            }
+        }, YEAR, MONTH, DATE);
+
+        datePickerDialog.show();
+    }
+}
--- app/src/main/java/com/example/qrscanner_appproject/patientsDetailsFragment.java
+++ app/src/main/java/com/example/qrscanner_appproject/patientsDetailsFragment.java
@@ -3,14 +3,20 @@ package com.example.qrscanner_appproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class patientsDetailsFragment extends Fragment {
 
+    Button addNewMeasurementsButton;
+
+    //Fragments
+    patientsAddMeasurements patientsAddMeasurements = new patientsAddMeasurements();
 
 
     @Override
@@ -23,6 +29,18 @@ public class patientsDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patients_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_patients_details, container, false);
+        addNewMeasurementsButton = view.findViewById(R.id.addNewMeasurementsButton);
+
+        addNewMeasurementsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+                transaction.replace(R.id.fragmentContainer, patientsAddMeasurements ); // give your fragment container id in first parameter
+                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                transaction.commit();
+            }
+        });
+        return view;
     }
 }
